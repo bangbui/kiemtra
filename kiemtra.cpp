4329c6c2e0b349c574613e6dd740d030a34a32e1
duongquoctai <duongquoctai153@gmail.com>
2017-12-28 15:00:29 +0700

    Dương Quốc Tài

--- kiemtra.cpp
+++ kiemtra.cpp
@@ -1,17 +1,19 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-int main()
+void main()
 {
 	int n;
-	cout<<"Nhap n: ";
-	cin>>n;
-	int i=2;
-	while(n>0 && i<=n)
+	cout << "Nhap n: ";
+	cin >> n;
+	int i = 2;
+	while (n>1)
 	{
-		if(n%i==0)
-			cout<<i<<" ";
-		i++;
+		if (n%i == 0)
+		{
+			cout << i << "*";
+			n = n / i;
+		}
+		else
+			i++;
 	}
-	return 0;
 }
