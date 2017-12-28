e595af21bc1972c7d5f25942f0330b7982b2eeb5
vanthang1234 <nguyenvanthang15011996@gmail.com>
2017-12-28 14:49:47 +0700

    Nguyễn Văn Thắng update code

--- kiemtra.cpp
+++ kiemtra.cpp
@@ -1,16 +1,36 @@
 #include <iostream>
 using namespace std;
 
+bool KiemTra(int n)
+{
+	if (n == 2)
+		return true;
+	if (n % 2 == 0)
+		return false;
+	for (int i = 3; i <= sqrt(n); i += 2)
+	{
+		if (n % i == 0)
+			return false;
+	}
+	return true;
+}
+
 int main()
 {
 	int n;
-	cout<<"Nhap n: ";
-	cin>>n;
-	int i=2;
-	while(n>0 && i<=n)
+	cout << "Nhap n: ";
+	cin >> n;
+	int i = 2;
+	while (n>0 && i <= n)
 	{
-		if(n%i==0)
-			cout<<i<<" ";
+		for (int j = i;;)
+			if (n%j == 0 && KiemTra(j))
+			{
+			cout << j << " ";
+			n /= j;
+			}
+			else
+				break;
 		i++;
 	}
 	return 0;
