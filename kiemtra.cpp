c08b44235d7d213d4aa85b5c76177b715a5b0ce1
NhatTruong <truong.nguyennhat96@gmail.com>
2017-12-28 14:56:52 +0700

    Nguyen Nhat Truong Commit file

--- kiemtra.cpp
+++ kiemtra.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
 using namespace std;
 
+void PhanTich(int n)
+{
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = i;;)
+		{
+			if (n % j == 0)
+			{
+				cout << j << " * ";
+				n /= j;
+			}
+			else
+				break;
+		}
+	}
+}
 int main()
 {
 	int n;
-	cout<<"Nhap n: ";
-	cin>>n;
-	int i=2;
-	while(n>0 && i<=n)
-	{
-		if(n%i==0)
-			cout<<i<<" ";
-		i++;
-	}
+	cout << "Nhap n: ";
+	cin >> n;
+	int i = 2;
+	PhanTich(n);
 	return 0;
 }
