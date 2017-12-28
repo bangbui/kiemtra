a690b334a95bf376bbf9edb49846dcb5cfbb30aa
nguyen viet thang <https://github.com/bangbui/kiemtra.git>
2017-12-28 14:58:16 +0700

    Nguyen Viet Thang

--- kiemtra.cpp
+++ kiemtra.cpp
@@ -4,14 +4,32 @@ using namespace std;
 int main()
 {
 	int n;
-	cout<<"Nhap n: ";
-	cin>>n;
-	int i=2;
-	while(n>0 && i<=n)
+	cout << "Nhap n: ";
+	cin >> n;
+	int i = 2;
+	while (n > 0 && i <= n)
 	{
-		if(n%i==0)
-			cout<<i<<" ";
+		if (n%i == 0)
+		{
+					if (kiemTraSoNguyenTo(i)==0)
+						cout << i << " ";
+		}
 		i++;
 	}
 	return 0;
 }
+	int kiemTraSoNguyenTo(int n)
+	{
+		bool flag = false;
+
+		for (int i = 2; i <= n / 2; ++i)
+		{
+			if (n%i == 0)
+			{
+				flag = true;
+				break;
+			}
+		}
+		return flag;
+	}
+
