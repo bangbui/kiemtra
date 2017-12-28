a793a3edc8d37d6e00af33df0069f679b2eed81b
Huynhhagiang <hagiangstu@gmail.com>
2017-12-28 15:18:41 +0700

    đã xuất tích các số nguyên tố của n

---
+++ HuynhHaGiang.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+void PhanTich(int n)
+{
+	cout << "tich cac so nguyen to cua" << n << " la : " << n << ":=";
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = i;;)
+		{
+			if (n % j == 0)
+			{
+				cout << j << " * ";
+				n /= j;
+			}
+			else
+				break;
+		}
+	}
+}
+void main()
+{
+	int n;
+	cout << "nhap so can kiem tra :";
+	cin >> n;
+	PhanTich(n);
+
+}
