177ed33877e19403b721d9f1b0d33992f02cd15f
duy <phamduy17296@gmail.com>
2017-12-28 15:12:42 +0700

    phamquocduy

--- kiemtra.cpp
+++ kiemtra.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
 	int n;
+	int tich = 1;
 	cout<<"Nhap n: ";
 	cin>>n;
 	int i=2;
@@ -13,5 +14,14 @@ int main()
 			cout<<i<<" ";
 		i++;
 	}
+	cout << "tich 2 so nguyen la:" << tich << endl;
 	return 0;
+	system("pause");
+}
+int tich(int n)
+{
+	int tich = 1;
+	for (int i = 0; i <= n; i++)
+		tich = tich*i;
+	return tich;
 }
