328d081d336aedb341690d77d64bc9a5d23d81fb
HanhNhu123456 <nhutrieuit25@gmail.com>
2017-12-28 15:10:07 +0700

    Trieu Ngo Hanh Nhu

--- kiemtra.cpp
+++ kiemtra.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 using namespace std;
 
+
+
+void PhanTich(int n)
+{
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = i;;)
+		{
+			if (n % j == 0 && KiemTra(j))
+			{
+				cout << j << " * ";
+				n /= j;
+			}
+			else
+				break;
+		}
+	}
+}
+
 int main()
 {
 	int n;
-	cout<<"Nhap n: ";
-	cin>>n;
-	int i=2;
-	while(n>0 && i<=n)
+	cout << "Nhap n: ";
+	cin >> n;
+	int i = 2;
+	while (n>0 && i <= n)
 	{
-		if(n%i==0)
-			cout<<i<<" ";
+		if (n%i == 0)
+			cout << i << " ";
 		i++;
 	}
+	PhanTich(n);
 	return 0;
 }
